323f97f2b4238fa35b5a91011b2cb02ac887fb19
Cristian <crdcaro01@gmail.com>
2023-08-17 20:04:47 -0500

    agrgado de info

--- js/main.js
+++ js/main.js
@@ -7,10 +7,28 @@ const cards = async() => {
 
         data.results.forEach(card => {
            html += `
-            <div class = "card">
-                <img src="${card.image}" alt="image of character">
-                <h2>${card.name}</h2>
-            </div>
+           <div class="container-card">
+    
+           <img src="${card.image}" alt="image of character" class="image">
+         <div class="container-text">
+           <h3>${card.name}</h3>
+           <p>${card.status} - ${card.species}</p>
+           <div class="container3">
+             <div class="div-1">
+              <a href="${card.episode}">Episode</a>
+             </div>
+             <div class="div-2">
+                <a href="https://rickandmortyapi.com/api/location">${card.location.name}</a>
+             </div>
+             
+           </div>
+           <div class="container4">
+             <img src="images/image-avatar.png" alt="image-avatar">
+             <p>Creation of <span> Jules Wyvern</span></p>
+           </div>
+           
+         </div>
+       </div>
             `
             
         });
